044c930af06aed9e8ae929ddf93653b022da1a28
zhh <617146873@qq.com>
2016-12-09 22:40:21 +0800

    添加矩阵的减法、加法和转置

--- algorithm_final_project/main.cpp
+++ algorithm_final_project/main.cpp
@@ -3,6 +3,7 @@
 #include<iomanip>
 #include<cassert>
 #include<cstdlib>
+#include<ctime>
 using namespace std;
 
 // 矩阵
@@ -43,7 +44,7 @@ public:
 
 	~mat()
 	{
-		cout << "释放内存" << endl;
+		cout << "释放内存 size = " << n << endl;
 		for (int i = 0;i < n;i++)
 			delete[] m_matrix[i];
 		delete[] m_matrix;
@@ -89,7 +90,9 @@ public:
 // 用随机数初始化矩阵
 void init_mat(mat &m)
 {
-	srand(time(0));
+	static auto seed = time(nullptr);
+	seed++;
+	srand(seed);
 	int n = m.n;
 	for (int i = 0;i < n;i++)
 		for (int j = 0;j < n;j++)
@@ -98,10 +101,10 @@ void init_mat(mat &m)
 
 // 矩阵相乘，返回一个矩阵
 // 一般算法
-mat mutiply_mat(mat &m1, mat &m2)
+mat mutiply_mat(mat &m1,mat &m2)
 {
 	assert(m1.n == m2.n);
-
+	auto start_time = clock();
 	int n = m1.n;
 	mat result(n);
 
@@ -112,18 +115,82 @@ mat mutiply_mat(mat &m1, mat &m2)
 			int s = 0;
 			for (int k = 0;k < n;k++)
 			{
-				s = s + m1[i][k] * m2[k][j];
+				s += m1[i][k] * m2[k][j];
 			}
 			result[i][j] = s;
 		}
 	}
+	cout << "exm?" << endl;
+	cout << "time: " << clock() - start_time << endl;
+	return result;
+}
 
+// 矩阵加法
+mat add_mat(mat &m1, mat &m2)
+{
+	int n = m1.n;
+	mat result(n);
+
+	for (int i = 0;i < n;i++)
+		for (int j = 0;j < n;j++)
+			result[i][j] = m1[i][j] + m2[i][j];
+
+	return result;
+}
+
+// 矩阵减法
+mat sub_mat(mat &m1, mat &m2)
+{
+	int n = m1.n;
+	mat result(n);
+
+	for (int i = 0;i < n;i++)
+		for (int j = 0;j < n;j++)
+			result[i][j] = m1[i][j] - m2[i][j];
+
+	return result;
+}
+
+// 转置矩阵
+mat transpose_mat(mat &m)
+{
+	int n = m.n;
+	mat result(n);
+	for (int i = 0;i < n;i++)
+		for (int j = 0;j < n;j++)
+			result[j][i] = m[i][j];
+	return result;
+}
+
+// strassen方法
+mat strassen_mutiply_mat_version1(const mat &m1, const mat &m2)
+{
+	mat result(0);
 	return result;
 }
 
 int main()
 {
-	mat m(4);
+	//auto start_time = clock();
+	//mat m1(1000);
+	//mat m2(1000);
+	//init_mat(m1);
+	//init_mat(m2);
+	//cout << "time: " << clock() - start_time << endl;
+	//cout << m1 << endl << m2 << endl;
+	//cout << mutiply_mat(m1, m2) << endl;
+	//cout << "time: " << clock() - start_time << endl;
+
+	mat m1(5), m2(5);
+	init_mat(m1);
+	init_mat(m2);
+
+	cout << m1 << endl;
+	mat m3 = transpose_mat(m1);
+	cout << m3 << endl;
+	mat m4 = transpose_mat(m3);
+	cout << (m4 == m1) << endl;
+
 
 	system("pause");
 	return 0;
