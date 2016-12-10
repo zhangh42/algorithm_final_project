748865b0fa1e2ea19cf5e35aec380e08bb0aad64
zhh <617146873@qq.com>
2016-12-10 17:08:06 +0800

    完成第一个版本的strassen矩阵乘法，但是效率比较低，比传统算法还慢

--- algorithm_final_project/main.cpp
+++ algorithm_final_project/main.cpp
@@ -19,11 +19,15 @@ public:
 		m_matrix = new int*[n];
 		for (int i = 0;i < n;i++)
 			m_matrix[i] = new int[n];
+
+		for (int i = 0;i < n;i++)
+			for (int j = 0;j < n;j++)
+				m_matrix[i][j] = 0;
 	}
 
 	mat(mat& m)
 	{
-		cout << "mat copy happen..." << endl;
+		//cout << "mat copy happen..." << endl;
 		n = m.n;
 		m_matrix = new int*[n];
 		for (int i = 0;i < n;i++)
@@ -37,14 +41,14 @@ public:
 	mat(mat &&m) noexcept
 		:n(m.n), m_matrix(m.m_matrix)
 	{
-		cout << "right value copy" << endl;
+		//cout << "right value copy" << endl;
 		m.m_matrix = nullptr;
 		m.n = 0;
 	}
 
 	~mat()
 	{
-		cout << "释放内存 size = " << n << endl;
+		//cout << "释放内存 size = " << n << endl;
 		for (int i = 0;i < n;i++)
 			delete[] m_matrix[i];
 		delete[] m_matrix;
@@ -120,8 +124,6 @@ mat mutiply_mat(mat &m1,mat &m2)
 			result[i][j] = s;
 		}
 	}
-	cout << "exm?" << endl;
-	cout << "time: " << clock() - start_time << endl;
 	return result;
 }
 
@@ -163,35 +165,135 @@ mat transpose_mat(mat &m)
 }
 
 // strassen方法
-mat strassen_mutiply_mat_version1(const mat &m1, const mat &m2)
+mat strassen_mutiply_mat_version1(mat &m1, mat &m2)
 {
-	mat result(0);
+	assert(m1.n == m2.n);
+
+	int n = m1.n;
+	if (n <= 25)
+		return mutiply_mat(m1, m2);
+
+	mat result(n);
+	// 是否是2的整数倍
+	int half_n = n / 2;
+	if (n % 2 == 1)
+		half_n++;
+	int up_left = n / 2;
+	int down_right = n / 2;
+	if (n % 2 == 1)
+		up_left++;
+
+	// 复制m1
+	mat A11(half_n), A12(half_n), A21(half_n), A22(half_n);
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < up_left;j++)
+		{
+			A11[i][j] = m1[i][j];
+		}
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < down_right;j++)
+			A12[i][j] = m1[i][j + up_left];
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < up_left;j++)
+		{
+			A21[i][j] = m1[i + up_left][j];
+		}
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < down_right;j++)
+			A22[i][j] = m1[i + up_left][j + up_left];
+
+	// 复制m2
+	mat B11(half_n), B12(half_n), B21(half_n), B22(half_n);
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < up_left;j++)
+		{
+			B11[i][j] = m2[i][j];
+		}
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < down_right;j++)
+			B12[i][j] = m2[i][j + up_left];
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < up_left;j++)
+		{
+			B21[i][j] = m2[i + up_left][j];
+		}
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < down_right;j++)
+			B22[i][j] = m2[i + up_left][j + up_left];
+
+	// 7次乘法
+	mat M1 = strassen_mutiply_mat_version1(
+		sub_mat(A12, A22), add_mat(B21, B22)
+	);
+	mat M2 = strassen_mutiply_mat_version1(
+		add_mat(A11, A22), add_mat(B11, B22)
+	);
+	mat M3 = strassen_mutiply_mat_version1(
+		sub_mat(A11, A21), add_mat(B11, B12)
+	);
+	mat M4 = strassen_mutiply_mat_version1(
+		add_mat(A11, A12), B22
+	);
+	mat M5 = strassen_mutiply_mat_version1(
+		A11, sub_mat(B12, B22)
+	);
+	mat M6 = strassen_mutiply_mat_version1(
+		A22, sub_mat(B21, B11)
+	);
+	mat M7 = strassen_mutiply_mat_version1(
+		add_mat(A21, A22), B11
+	);
+
+	mat C1 = add_mat(
+		M1, add_mat(M2, sub_mat(
+			M6, M4
+		))
+	);
+	mat C2 = add_mat(M4, M5);
+	mat C3 = add_mat(M6, M7);
+	mat C4 = add_mat(
+		M2, sub_mat(M5, add_mat(
+			M3, M7
+		))
+	);
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < up_left;j++)
+		{
+			result[i][j] = C1[i][j];
+		}
+	for (int i = 0;i < up_left;i++)
+		for (int j = 0;j < down_right;j++)
+			result[i][j + up_left] = C2[i][j];
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < up_left;j++)
+			result[i+up_left][j] = C3[i][j];
+	for (int i = 0;i < down_right;i++)
+		for (int j = 0;j < down_right;j++)
+			result[i + up_left][j + up_left] = C4[i][j];
 	return result;
 }
 
 int main()
 {
-	//auto start_time = clock();
-	//mat m1(1000);
-	//mat m2(1000);
-	//init_mat(m1);
-	//init_mat(m2);
-	//cout << "time: " << clock() - start_time << endl;
-	//cout << m1 << endl << m2 << endl;
-	//cout << mutiply_mat(m1, m2) << endl;
-	//cout << "time: " << clock() - start_time << endl;
+	auto start_time = clock();
 
-	mat m1(5), m2(5);
+	
+	int n = 500;
+	mat m1(n), m2(n);
 	init_mat(m1);
 	init_mat(m2);
 
-	cout << m1 << endl;
-	mat m3 = transpose_mat(m1);
+	mat m3 = mutiply_mat(m1, m2);
+	auto mid = clock();
+	mat m4 = strassen_mutiply_mat_version1(m1, m2);
+	auto end = clock();
+
 	cout << m3 << endl;
-	mat m4 = transpose_mat(m3);
-	cout << (m4 == m1) << endl;
-
-
+	cout << m4 << endl;
+	cout << "time: " << mid - start_time << endl;
+	cout << "time: " << end - start_time << endl;
+	cout << (m3 == m4) << endl;
+	
 	system("pause");
 	return 0;
 }
